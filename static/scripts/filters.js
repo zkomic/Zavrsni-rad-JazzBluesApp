d6927d1db5a57879d9ae9582f95fc4e469576f81
Zrinka <zrinka.komic@gmail.com>
2022-01-10 11:31:11 +0100

    filters design

--- static/scripts/filters.js
+++ static/scripts/filters.js
@@ -7,3 +7,15 @@ document.querySelector('.close').addEventListener('click',
 function() {
     document.querySelector('.bg-modal-filters').style.display = 'none';
 });
+
+const loadMoreBtn = document.querySelector(".load-more-btn");
+const text = document.querySelector(".artists");
+
+loadMoreBtn.addEventListener("click", (e) => {
+  text.classList.toggle("show-more");
+  if (loadMoreBtn.innerText === "Read More") {
+    loadMoreBtn.innerText = "Read Less";
+  } else {
+    loadMoreBtn.innerText = "Read More";
+  }
+});
